4ef740f67e0fd270ddcc1e0525d709ac1e13fd9d
Adan1996 <syahdanmasyhuri@gmail.com>
2020-11-07 14:16:47 +0700

    Basic styling react native component

--- App.js
+++ App.js
@@ -1,11 +1,80 @@
-import React, { Component } from 'react';
-import {Image, Text, TextInput, View} from 'react-native';
+/* eslint-disable react-native/no-inline-styles */
+import React, {Component} from 'react';
+import {Image, StyleSheet, Text, TextInput, View} from 'react-native';
+import AndroidSecurity from './Android-Securitry.jpg';
 
 // functional component
 const App = () => {
+  return <SampleReactNativeComponent />;
+};
+
+const SampleReactNativeComponent = () => {
   return (
     <View>
-      <View style={{width: 80, height: 80, backgroundColor: '#0984e3'}}/>
+      <Text style={styles.text}>Sample React Native Component</Text>
+      <View
+        style={{
+          width: 100,
+          height: 100,
+          backgroundColor: '#0984e3',
+          borderWidth: 2,
+          borderColor: '#5f27cd',
+          marginTop: 20,
+          marginLeft: 20,
+        }}
+      />
+      <View
+        style={{
+          padding: 12,
+          backgroundColor: '#f2f2f2',
+          width: 212,
+          borderRadius: 8,
+        }}>
+        <Image
+          source={AndroidSecurity}
+          style={{width: 188, height: 107, borderRadius: 8}}
+        />
+        <Text style={{fontSize: 14, fontWeight: 'bold', marginTop: 16}}>
+          Android Apps For All
+        </Text>
+        <Text style={{fontSize: 12, fontWeight: 'bold', color: '#F2994A'}}>
+          Rp. 12.000
+        </Text>
+        <Text style={{fontSize: 12, fontWeight: '300', marginTop: 12}}>
+          Kukusan Depok
+        </Text>
+        <View
+          style={{backgroundColor: '#6FCF97', borderRadius: 25, marginTop: 20}}>
+          <Text
+            style={{
+              fontSize: 14,
+              fontWeight: '600',
+              color: 'white',
+              textAlign: 'center',
+            }}>
+            Beli
+          </Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+// membuat styling terpisah
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#10ac84',
+    marginLeft: 20,
+    marginTop: 40,
+  },
+});
+
+const SampleComponent = () => {
+  return (
+    <View>
+      <View style={{width: 80, height: 80, backgroundColor: '#0984e3'}} />
       <Text>Syahdan</Text>
       <Masyhuri />
       <Photo />
@@ -14,15 +83,20 @@ const App = () => {
       <Profile />
     </View>
   );
-}
+};
 
 const Masyhuri = () => {
   return <Text>Masyhuri</Text>;
-}
+};
 
 const Photo = () => {
-  return <Image source={{uri: 'https://placeimg.com/100/100/tech'}} style={{width: 100, height: 100}}/>;
-}
+  return (
+    <Image
+      source={{uri: 'https://placeimg.com/100/100/tech'}}
+      style={{width: 100, height: 100}}
+    />
+  );
+};
 
 // class component
 class BoxGreen extends Component {
@@ -35,7 +109,10 @@ class Profile extends Component {
   render() {
     return (
       <View>
-        <Image source={{uri: 'https://placeimg.com/100/100/animals'}} style={{width: 100, height: 100, borderRadius: 50}}/>
+        <Image
+          source={{uri: 'https://placeimg.com/100/100/animals'}}
+          style={{width: 100, height: 100, borderRadius: 50}}
+        />
         <Text style={{color: 'blue'}}>Ini hewan</Text>
       </View>
     );
