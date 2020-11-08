cf25a6394cb0da63c9c9ba3f84a28de92439a7e5
Adan1996 <syahdanmasyhuri@gmail.com>
2020-11-08 10:44:37 +0700

    Memahami Basic Layouting di React Native dengan menggunakan FlexBox

--- App.js
+++ App.js
@@ -3,14 +3,16 @@ import {ScrollView, View} from 'react-native';
 
 import SampleComponent from './SampleComponent';
 import SampleReactNativeComponent from './SampleReactNativeComponent';
+import MateriFlexBox from './MateriFlexBox';
 
 // functional component
 const App = () => {
   return (
     <View>
       <ScrollView>
-        <SampleComponent />
-        <SampleReactNativeComponent />
+        {/* <SampleComponent /> */}
+        {/* <SampleReactNativeComponent /> */}
+        <MateriFlexBox />
       </ScrollView>
     </View>
   );
---
+++ MateriFlexBox.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import {Image, Text, View} from 'react-native';
+
+class MateriFlexBox extends Component {
+    render() {
+        return (
+            <View>
+                <View style={{flexDirection: 'row', backgroundColor: '#c8d6e5', alignItems: 'center', justifyContent: 'space-between'}}>
+                    <View backgroundColor='#ee5253' style={{width: 50, height: 50}}/>
+                    <View backgroundColor='#feca57' style={{width: 50, height: 50}}/>
+                    <View backgroundColor='#1dd1a1' style={{width: 50, height: 50}}/>
+                    <View backgroundColor='#5f27cd' style={{width: 50, height: 50}}/>
+                </View>
+                <View style={{flexDirection: 'row', justifyContent: 'space-around'}}>
+                    <Text>Beranda</Text>
+                    <Text>Video</Text>
+                    <Text>Playlist</Text>
+                    <Text>Komunitas</Text>
+                    <Text>Channel</Text>
+                    <Text>Tentang</Text>
+                </View>
+                <View style={{flexDirection: 'row', alignItems: 'center', marginTop: 20}}>
+                    <Image source={require('./anonymouse.jpeg')} style={{width: 100, height: 100, borderRadius: 50, marginRight: 20}} />
+                    <View>
+                        <Text style={{fontSize: 20, fontWeight: 'bold'}}>Anonymouse</Text>
+                        <Text>100 ribu subscriber</Text>
+                    </View>
+                </View>
+            </View>
+        )
+    }
+}
+
+export default MateriFlexBox;
